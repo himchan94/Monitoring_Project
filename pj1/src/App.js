649e39a532dfabfed46e7798180c86ca02fbfd1d
healthcarefrontend <himchan9494@gmail.com>
2021-09-01 20:02:38 +0900

    completed

--- pj1/src/App.js
+++ pj1/src/App.js
@@ -8,6 +8,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import NewProduct from "./pages/newProdct/NewProduct";
+import Product from "./pages/product/Product";
 
 function App() {
   return (
@@ -23,11 +26,21 @@ function App() {
             <Route path="/users">
               <UserList />
             </Route>
-            <Route path="/user/userId">
+            <Route path="/user/:userId">
               <User />
             </Route>
             <Route path="/newUser">
               <NewUser />
+            </Route>
+
+            <Route path="/products">
+              <ProductList />
+            </Route>
+            <Route path="/product/:productId">
+              <Product />
+            </Route>
+            <Route path="/newproduct">
+              <NewProduct />
             </Route>
           </Switch>
         </div>
